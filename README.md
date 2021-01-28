77aec34a1327cb4f0ee5df916aa2da0905dd510f
Jordan Cotter <jrdn.cttr@gmail.com>
2021-01-28 08:40:45 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# simple-express-boilerplate
+
+## Start
+`git clone git@github.com:cotterjd/simple-express-boilerplate.git`<br>
+`npm install` <br>
+`npm start`<br>
+
+## Use
+`curl http://localhost:3000`
---
+++ server.js
@@ -0,0 +1,15 @@
+const { log } = console
+, app = require('express')()
+, bodyParser = require('body-parser')
+;
+
+app.use(bodyParser.json()) // to be able to receive json in requests
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.set('port', 3000)
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
