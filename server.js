b69de8de7c6116d07d4100ce2065d2c46b72228f
Jordan Cotter <jrdn.cttr@gmail.com>
2021-01-28 08:48:28 -0600

    add package-lock.json and change port to 3040

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.set('port', 3000)
+app.set('port', 3040)
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
